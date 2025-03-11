609cc38e0ec060fde2bd925a18ec574e6bbba284
Veljko Nedeljković <143710575+bonkooo@users.noreply.github.com>
2025-03-11 16:23:34 +0100

    Added header and router for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import Header from './Components/Header/header';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Pages/home.jsx';
+import Login from './Pages/login.jsx';
+import Chat from './Pages/chat.jsx'
+import Assignments from './Pages/assignments.jsx'
 
 function App() {
   return (
-    <Header/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/assignments" element={<Assignments />} />
+        
+      </Routes>
+    </Router>
   );
 }
 
--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import './header.css';
+import logo from '../../Assets/logo.png';
+import applyIcon from '../../Assets/applyIcon.png';
+
 
 const Header = () => {
     return (
         <header className="header">
             <div className="logo">
-                <img src="" alt="" />
+                <img src={logo} alt="" />
             </div>
-            <nav className="nav-links">
-                <div className="nav-btn"><a href="#home">Home</a></div>
-                <div className="nav-btn"><a href="#home">Home</a></div>
-                <div className="nav-btn"><a href="#home">Home</a></div>
-                <div className="nav-btn"><a href="#home">Home</a></div>
-            </nav>
-            <button className="cta-button">Get Started</button>
+            <div className="navBtns">
+                <div className="nav-btn">Moji četovi</div>
+                <div className="nav-btn">Zaduženja</div>
+                <div className="nav-btn">Redarstva</div>
+                <div className="nav-btn">Test ličnosti</div>
+                <div className="nav-btn">Nagrade</div>
+                <div className="nav-btn">Vesti</div>
+            </div>
+            <div className="cta-button">
+                <img className='applyIcon' src={applyIcon} alt="" />
+                <h1 className='ctaText'>Prijava</h1>
+                </div>
         </header>
     );
 };
